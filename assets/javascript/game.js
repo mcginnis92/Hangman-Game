51579d41487454d6eb0132114a8a2a4bcc0f4cbd
Molly Moran <mollyclaremoran@gmail.com>
2018-01-19 09:23:59 -0600

    javascriptv1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,22 @@
-//Use the `src` attribute of the `script` tag to link to this file, rather than embedding the code directly in your HTML document.
+//set up new array for new word
+var hedwig = ['h', 'e', 'd', 'w', 'i', 'g'];
+
+//set up new count of lives for new word
+var countLives = 10;
+
+//something to listen for user key input or prompt
+var letter1 = prompt('Input a letter.');
+console.log(letter1);
+
+//compare inputted letters to letters in array
+if (hedwig.includes(letter1)) {
+  
+  //IF CORRECT: show letter 1 in the box
+  console.log(letter1);
+}
+  //IF INCORRECT: remove 1 life
+  else {
+  countLives--;
+  console.log(countLives);
+}
+
