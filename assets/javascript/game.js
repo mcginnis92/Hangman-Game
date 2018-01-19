bd92ec3466aba06f43a792fb79f55c3fa150aee9
Molly Moran <mollyclaremoran@gmail.com>
2018-01-19 10:02:32 -0600

    javascript2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,19 +4,30 @@ var hedwig = ['h', 'e', 'd', 'w', 'i', 'g'];
 //set up new count of lives for new word
 var countLives = 10;
 
-//something to listen for user key input or prompt
-var letter1 = prompt('Input a letter.');
-console.log(letter1);
-
-//compare inputted letters to letters in array
-if (hedwig.includes(letter1)) {
+//if number of remaining lives > 0, try again
+while (countLives > 0) {
   
-  //IF CORRECT: show letter 1 in the box
+  //something to listen for user key input or prompt
+  var letter1 = prompt('Input a letter.');
   console.log(letter1);
-}
+
+  //compare inputted letters to letters in array
+  if (hedwig.includes(letter1)) {
+  
+  //IF CORRECT: show letter 1 on the screen
+  console.log(letter1);
+  }
+  
   //IF INCORRECT: remove 1 life
   else {
   countLives--;
   console.log(countLives);
+  }
+  
 }
 
+//is word complete?
+
+//if complete, go to next word
+
+//if incomplete, repeat cycle
