f77d463f7c7d1ef80ceb3db4ba904064c957c9b5
Molly Moran <mollyclaremoran@gmail.com>
2018-01-22 16:10:29 -0600

    event-listener-working

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,61 +1,75 @@
-//create list of words and pull random word to the page.
+//potential words
 var potentialWords = ['hedwig', 'horcrux', 'patronus', 'snape', 'dumbledore', 'scabbers'];
+
+//chooses a word from the array
 var chosenWord = potentialWords[Math.floor(Math.random()* potentialWords.length)]; 
 console.log(chosenWord);
 
-//split up chosenWord into an array.
+//split up chosenWord into an array
 var wordArr = chosenWord.split('');
 console.log(wordArr);
 
 //generate dashes to display number of characters in the chosenWord 
-var displayHTML = '';
-
-for (var i = 0; i < wordArr.length; i++){
-  displayHTML += '_ ';
-}
-console.log(displayHTML);
-
+var displayBlanks = [];
+  for (var i = 0; i < wordArr.length; i++){
+    displayBlanks.push('_ ');
+  }
 var displayDashes = document.querySelector('#empty');
-  displayDashes.innerHTML = displayHTML;
+  displayDashes.innerHTML = displayBlanks;
 
+//RESET GAME
+var countLives = 10;
 
+//NEW GUESS FUNCTION
+document.addEventListener('keyup', logLetter);
 
-//This function is run whenever the user inputs a key as long as adequate lives remain.
-if (countLives > 0){  
+function logLetter(event){
+  var letter1 = event.key;
+  console.log(letter1);
+}
 
-//adds event listener for user to press a key
-document.addEventListener("keyup", userGuess);
-
-
-    var userGuess = function (event) {
-    var letter1 = event.key;
-    console.log(letter1);
-
-    var countLives = 10;
+  //only run if there are lives remaining
+  if (countLives > 0) {
     var displayLives = document.querySelector('#lives');
     displayLives.innerHTML = countLives;
+  
+  for (var i = 0; i < wordArr.length; i++){
+      //MATCH
+      if (letter1 === wordArr[i]){
+        displayBlanks[i]= letter1;
+        for (var i = 0; i < wordArr.length; i++) {
+          wordArr[i].innerText = correct1;
+        }
 
-
-
-
+      //NO MATCH
+      } else {
+      var incorrect1 = letter1.toUpperCase();
+      var displayIncorrect = document.querySelector('#incorrect');
+      displayIncorrect.innerText = incorrect1;
+      countLives--; //deducts life and displays number remaining
     }
-
-
-    //compare inputted letters to letters in array
+  }
+}
+//   //if match update the wordArr with the correct text
+//   var updateArray = function (correct1) {
+//   wordArr.innerText = correct1;
+//   for (var i = 0; i < wordArr.length; i++) {
+//   wordArr[i].innerText = correct1;
+//   }
+// }
+//     if (wordArr.includes(letter1)) {
+//       var correct1 = letter1.toUpperCase();
+//       console.log = correct1;
+     
+//       match = true;
+//       document.querySelector('#correct').innerText += letter1;
+      
+//       //add new code to rebuild the word array here
     
-
-}
-
-
-//if lives have run out, display full word. start over.
-else {
-
-}
+//   }
+//  }
 
 
-
-
-
-// //if complete, go to next word
-
-// if incomplete, repeat cycle
+    
+ 
+     
---
+++ game.js
@@ -0,0 +1,72 @@
+//potential words
+var potentialWords = ['hedwig', 'horcrux', 'patronus', 'snape', 'dumbledore', 'scabbers'];
+
+//chooses a word from the array
+var chosenWord = potentialWords[Math.floor(Math.random()* potentialWords.length)]; 
+console.log(chosenWord);
+
+//split up chosenWord into an array
+var wordArr = chosenWord.split('');
+console.log(wordArr);
+
+//generate dashes to display number of characters in the chosenWord 
+var displayHTML = [];
+
+for (var i = 0; i < wordArr.length; i++){
+  displayHTML.push('_');
+}
+console.log(displayHTML);
+
+var displayDashes = document.querySelector('#empty');
+  displayDashes.innerHTML = displayHTML;
+
+// //set countLives to 10
+// var countLives = 10;
+
+// //new loop to update the wordArr with the correct text
+// var updateArray = function (correct1) {
+//   wordArr.innerText = correct1;
+//   for (var i = 0; i < wordArr.length; i++) {
+//   wordArr[i].innerText = correct1;
+//   }
+// }
+
+// //user inputs a key
+// var logLetter = function(event){
+//   var letter1 = event.key;
+//   if (countLives > 0) {
+//     var displayLives = document.querySelector('#lives');
+//     displayLives.innerHTML = countLives;
+//     var match = false;
+      
+//     if (wordArr.includes(letter1)) {
+//       var correct1 = letter1.toUpperCase();
+//       console.log = correct1;
+     
+//       match = true;
+//       document.querySelector('#correct').innerText += letter1;
+      
+//       //add new code to rebuild the word array here
+//       wordArr = ['s', 'n', 'a', 'p', 'e']
+//       for (var i = 0, i< wordArr.length, i++){
+//         if (letter1 === wordArr[i]){
+//           displayHTML[i]= letter1;
+//         }
+//     }
+
+//     else {
+//       var incorrect1 = letter1.toUpperCase();
+//       var displayIncorrect = document.querySelector('#incorrect');
+//       displayIncorrect.innerText = incorrect1;
+      
+//       countLives--;
+//       var displayLives = document.querySelector('#lives');
+//       displayLives.innerText = countLives;
+//     }
+//   }
+//  }
+
+
+    
+ 
+     
